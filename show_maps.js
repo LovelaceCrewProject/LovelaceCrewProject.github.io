ba2b3d2c027f4a89205a7db6dd6b72347c9dddf2
alicebizeul <alice.bizeul@epfl.ch>
2018-12-15 21:29:25 +0100

    Debug

---
+++ show_maps.js
@@ -0,0 +1,20 @@
+const maps = [
+    //"maps/static_event.html",
+    //"maps/static_mention.html",
+    "maps/dynamic_event.html",
+    "maps/dynamic_mention.html",
+];
+
+
+function showMap(idx) {
+    const i = parseInt(idx);
+    d3.select(`#map${i}`)
+        .append('object')
+        .attr('data', maps[i])
+        .attr('width', "60%")
+        .attr('height', "900")
+        .attr('class', 'center-block');
+
+    d3.select(`#button${i}`)
+        .remove();
+}
